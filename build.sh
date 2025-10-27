54b904b6ea164c84c71369d0a6e415badafad3ed
Yuta Saito <kateinoigakukun@gmail.com>
2025-10-27 16:26:59 +0900

    Fix index.d.ts packaging

--- build.sh
+++ build.sh
@@ -8,3 +8,4 @@ env JAVASCRIPTKIT_EXPERIMENTAL_BRIDGEJS=1 swift package --swift-sdk "$SWIFT_SDK_
 npx esbuild --bundle --format=esm --external:@bjorn3/browser_wasi_shim --platform=node src/index.node.js --outfile=dist/index.node.js
 npx esbuild --bundle --format=esm --external:@bjorn3/browser_wasi_shim --platform=browser src/index.browser.js --outfile=dist/index.browser.js
 cp ./.build/plugins/PackageToJS/outputs/Package/swift-demangle.wasm dist/swift-demangle.wasm
+cp ./src/index.d.ts dist/index.d.ts
